c8da2209aa2bfc93448c96551cfe21a4ae1a7507
alzheimeer <fogniebla@hotmail.com>
2021-03-02 08:54:13 -0800

    empty files

---
+++ 0x00-red_black_tree/rb_trees.h
@@ -0,0 +1,64 @@
+#ifndef C7C3570E_179A_4EA8_AACD_039567CD4226
+#define C7C3570E_179A_4EA8_AACD_039567CD4226
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * enum rb_color_e - Possible color of a Red-Black tree
+ *
+ * @RED: 0 -> Red node
+ * @BLACK: 1 -> Black node
+ * @DOUBLE_BLACK: 2 -> Double Black node
+ */
+typedef enum rb_color_e
+{
+	RED = 0,
+	BLACK,
+	DOUBLE_BLACK
+} rb_color_t;
+
+/**
+ * struct rb_tree_s - Red-Black tree node structure
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @color: Color of the node (RED or BLACK)
+ */
+typedef struct rb_tree_s
+{
+	int n;
+	rb_color_t color;
+	struct rb_tree_s *parent;
+	struct rb_tree_s *left;
+	struct rb_tree_s *right;
+} rb_tree_t;
+
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+void rb_tree_print(const rb_tree_t *tree);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* C7C3570E_179A_4EA8_AACD_039567CD4226 */
