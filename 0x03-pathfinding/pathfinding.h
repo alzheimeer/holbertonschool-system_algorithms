d31d2fde5a9d1ff5b71df91c685129da0166567a
Edgar Carlos M. Quintero <fogniebla@hotmail.com>
2021-05-23 16:10:57 -0500

    fix

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -1,8 +1,29 @@
-#ifndef PATHFINDING_H
-#define PATHFINDING_H
+#ifndef D57937A9_B092_4C05_8A99_588D2A08DFEE
+#define D57937A9_B092_4C05_8A99_588D2A08DFEE
 
-#include "queues.h"
+#define NUM_DIRECTIONS 4
 #include "graphs.h"
+#include "queues.h"
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+/**
+ * enum direction_s - which way to go
+ * @RIGHT: go east
+ * @BOTTOM: go down
+ * @LEFT: go west
+ * @TOP: go up
+ */
+enum direction_s
+{
+	RIGHT,
+	BOTTOM,
+	LEFT,
+	TOP
+};
 
 /**
  * struct point_s - Structure storing coordinates
@@ -18,9 +39,14 @@ typedef struct point_s
 
 queue_t *backtracking_array(char **map, int rows, int cols,
 							point_t const *start, point_t const *target);
+
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 							vertex_t const *target);
+
 queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 						vertex_t const *target);
+
 queue_t *a_star_graph(graph_t *graph, vertex_t const *start,
-					  vertex_t const *target);#endif /* PATHFINDING_H */
+					  vertex_t const *target);
+
+#endif /* D57937A9_B092_4C05_8A99_588D2A08DFEE */
