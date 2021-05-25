0810e4126029d516fd9db945a2f6dee5180b702b
alzheimeer <fogniebla@hotmail.com>
2021-05-24 20:39:30 -0700

    My Version

--- 0x03-pathfinding/0-backtracking_array.c
+++ 0x03-pathfinding/0-backtracking_array.c
@@ -14,11 +14,21 @@ queue_t *backtracking_array(char **map, int rows, int cols,
 			    point_t const *start, point_t const *target)
 {
 	queue_t *path;
-	int x, y;
+	int x, y, i, ret;
+	char **map2;
 
 	if (!map || !*map || !start || !target || rows < 1 || cols < 1)
 		return (NULL);
-
+	map2 = malloc(rows * sizeof(char *));
+	if (!map2)
+		exit(1);
+	for (i = 0; i < rows; i++)
+	{
+		map2[i] = malloc(cols + 1);
+		if (!map2[i])
+			exit(1);
+		strcpy(map2[i], map[i]);
+	}
 	path = queue_create();
 	if (path == NULL)
 		return (0);
@@ -28,8 +38,12 @@ queue_t *backtracking_array(char **map, int rows, int cols,
 		return (0);
 	x = start->x;
 	y = start->y;
-	backtracking((char **)map, 'Z', x, y, rows, cols, start, target, &path);
-
+	ret = backtracking(map2, 'Z', x, y, rows, cols, start, target, &path);
+	if (!ret)
+		queue_delete(path), path = NULL;
+	for (i = 0; i < rows; i++)
+		free(map2[i]);
+	free(map2);
 	return (path);
 }
 
@@ -45,6 +59,8 @@ int backtracking(char **map, char before, int x, int y,  int rows, int cols,
 	current->x = x;
 	current->y = y;
 	printf("Checking coordinates [%i, %i]\n", x, y);
+	map[y][x] = '1';
+
 	if (target->x == x && target->y == y)
 	{
 		queue_push_front(*path, current);
@@ -52,16 +68,22 @@ int backtracking(char **map, char before, int x, int y,  int rows, int cols,
 	}
 	if (x + 1 < cols)
 		if (map[y][x + 1] == '0' && before != 'l')
-			right = backtracking((char **)map, 'r', x + 1, y, rows, cols, start, target, path);
+			right = backtracking((char **)map, 'r', x + 1, y,
+					     rows, cols, start, target, path);
 	if (y + 1 < rows)
 		if (map[y + 1][x] == '0' && before != 'u' && right != 1)
-			bottom = backtracking((char **)map, 'd', x, y + 1, rows, cols, start, target, path);
+			bottom = backtracking((char **)map, 'd', x, y + 1,
+					      rows, cols, start, target, path);
 	if (x - 1 >= 0)
-		if (map[y][x - 1] == '0' && before != 'r' && right != 1 && bottom != 1)
-			left = backtracking((char **)map, 'l', x - 1, y, rows, cols, start, target, path);
+		if (map[y][x - 1] == '0' && before != 'r' && right != 1
+		    && bottom != 1)
+			left = backtracking((char **)map, 'l', x - 1, y,
+					    rows, cols, start, target, path);
 	if (y - 1 >= 0)
-		if (map[y - 1][x] == '0' && before != 'd' && right != 1 && bottom != 1 && left != 1)
-			up = backtracking((char **)map, 'u', x, y - 1, rows, cols, start, target, path);
+		if (map[y - 1][x] == '0' && before != 'd' && right != 1
+		    && bottom != 1 && left != 1)
+			up = backtracking((char **)map, 'u', x, y - 1,
+					  rows, cols, start, target, path);
 	if (right == 0 && bottom == 0 && left == 0 && up == 0)
 	{
 		return (0);
